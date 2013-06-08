a3a21dee263f522ef6163e5ef5d1ded33c490ac5
jeanlouispy <jeanlouispy@hotmail.com>
2013-06-08 17:30:53 +0200

    Signed-off-by: jeanlouispy <jeanlouispy@hotmail.com>

--- gen/com/example/aft_sms_project/R.java
+++ gen/com/example/aft_sms_project/R.java
@@ -11,7 +11,6 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int backGround=0x7f040001;
         public static final int text=0x7f040000;
     }
     public static final class drawable {
